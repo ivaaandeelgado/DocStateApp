8de5d15168f66dfd0e58f71ba1cb2215338a9554
ivanSF <idelgadoogalvan@gmail.com>
2025-11-13 20:22:36 +0100

    Constructor item

--- DocStateApp.Core/Modelos/Item.cs
+++ DocStateApp.Core/Modelos/Item.cs
@@ -8,7 +8,10 @@ namespace DocStateApp.Core.Modelos
     {
 
         public int Id { get; set; }
-        public string Nombre { get; set; } = string.Empty;
+        private string Usuario { get; set; } = string.Empty;
+        private string Pioridad { get; set; } = string.Empty;
+
+        //public string NombreArchivo { get; set; } = string.Empty;
         public Estado estado { get; set; }
         public string Descripcion { get; set; } = string.Empty;
 
